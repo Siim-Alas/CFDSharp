a32b3fbe1137ac2a756863da7519cba6e5980af6
Siim Alas <siim.alas@outlook.com>
2021-04-10 21:09:36 +0300

    Added inline documentation and a function for solving with Gaussian elimination to MatrixHelper.cs

--- CFDSharpClassLibrary/LinearAlgebra/MatrixHelpers.cs
+++ CFDSharpClassLibrary/LinearAlgebra/MatrixHelpers.cs
@@ -11,6 +11,16 @@ namespace CFDSharpClassLibrary.LinearAlgebra
     /// </summary>
     public static class MatrixHelpers
     {
+        /// <summary>
+        /// Finds the first (with the lowest index) row of a matrix that has a non-zero
+        /// element in the specified column.
+        /// </summary>
+        /// <param name="column">The column in which to look for non-zero elements.</param>
+        /// <param name="M">The matrix to search.</param>
+        /// <param name="startAtRow">The row at which the search is initiated.</param>
+        /// <returns>
+        /// The index of the first row in the matrix whith a non-zero element in the specified column.
+        /// </returns>
         public static int FindFirstRowOfNonZeroElementInColumn(int column, double[,] M, int startAtRow = 0)
         {
             int rows = M.GetLength(0);
@@ -23,6 +33,12 @@ namespace CFDSharpClassLibrary.LinearAlgebra
             }
             return -1;
         }
+        /// <summary>
+        /// Swaps the specified rows in the matrix. Note that the matrix will get mutated.
+        /// </summary>
+        /// <param name="M">The matrix whose rows to swap.</param>
+        /// <param name="row1">The index of the first row to swap.</param>
+        /// <param name="row2">The index of the second row to swap.</param>
         public static void SwapRows(ref double[,] M, int row1, int row2)
         {
             int columns = M.GetLength(1);
@@ -33,6 +49,12 @@ namespace CFDSharpClassLibrary.LinearAlgebra
                 M[row2, i] = temp;
             }
         }
+        /// <summary>
+        /// Transforms a matrix to unnormalized row echeon form using Gaussian elimination
+        /// (https://en.wikipedia.org/wiki/Gaussian_elimination). Note that this will mutate
+        /// the matrix.
+        /// </summary>
+        /// <param name="M">The matrix to be transformed.</param>
         public static void TransformInPlaceToRowEchelonForm(ref double[,] M)
         {
             int rows = M.GetLength(0);
@@ -80,6 +102,41 @@ namespace CFDSharpClassLibrary.LinearAlgebra
                 i++;
                 j++;
             }
+        }
+        /// <summary>
+        /// Solves the matrix equation Mv = b for the vector v with Gaussian elimination 
+        /// (https://en.wikipedia.org/wiki/Gaussian_elimination). The equation is given in the
+        /// form of a matrix with n rows and n + 1 columns, where the first n columns represent
+        /// the coefficients of the original matrix and the last column represents the vector b.
+        /// Note that this will mutate the input matrix.
+        /// </summary>
+        /// <param name="M">The n by n + 1 matrix representing the system of linear equations.</param>
+        /// <returns>An array representing the solution vector to the matrix equation.</returns>
+        public static double[] SolveWithGaussianElimination(ref double[,] M)
+        {
+            int rows = M.GetLength(0);
+            int columns = M.GetLength(1);
+
+            if (columns != rows + 1)
+            {
+                throw new ArgumentException(
+                    "The provided matrix did not have n rows and n + 1 columns.", nameof(M));
+            }
+
+            TransformInPlaceToRowEchelonForm(ref M);
+
+            double[] solution = new double[rows];
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                solution[i] = M[i, columns - 1];
+                for (int j = i + 1; j < rows; j++)
+                {
+                    solution[i] -= solution[j] * M[i, j];
+                }
+                solution[i] /= M[i, i];
+            }
+
+            return solution;
         }
     }
 }
